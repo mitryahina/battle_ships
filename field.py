6cf5d5f05804cb7560b9c0455e43fd0a94c5de0e
Yevhenia Mitryahina <mitryahina@ucu.edu.ua>
2018-03-10 11:37:47 +0200

    Update field.py

--- field.py
+++ field.py
@@ -4,12 +4,22 @@ from ship import Ship
 
 
 class Field:
+    """
+    Represents battleship field
+    """
     def __init__(self):
+        """
+        Initialize field with the list of ships, and the 
+        representation of field
+        """
         self.ships = generate_field()[0]
         self.field = generate_field()[1]
         self.shoots = set()
 
     def shoot_at(self, coordinates):
+        """
+        Perform shooting at one of the parts of the ship
+        """
         self.shoots.add(coordinates)
         for ship in self.ships:
             if ship.belong_to(coordinates):
@@ -17,7 +27,22 @@ class Field:
             else:
                 return "-"
 
+    def check_ships_killed(self):
+        """
+        Check if there are ships remaining
+        Return True if all are killed
+        """
+        count = 0
+        for ship in self.ships:
+            if ship.check_if_killed():
+                count += 1
+        return count == 10
+
     def field_without_ships(self):
+        """
+        Returns field representation for the enemy.
+        Show only shoots and landed ships.
+        """
         field = [[" " for i in range(10)] for j in range(10)]
         for line in range(10):
             for column in range(10):
@@ -26,6 +51,10 @@ class Field:
         return field
 
     def field_with_ships(self):
+        """
+        Return field representation for the current player.
+        Landed ships are marked with "x"
+        """
         field = self.field
         for shoot in self.shoots:
             if field[shoot[0]][shoot[1]] == '*':
@@ -34,6 +63,9 @@ class Field:
 
 
 def generate_field():
+    """
+    Function to randomly generate the field
+    """
     ships = []
     field = [[" " for i in range(10)] for i in range(10)]
     free_coordinates = [(i, j) for i in range(10) for j in range(10)]
@@ -58,6 +90,3 @@ def generate_field():
         for coordinates in ship:
             field[coordinates[0]][coordinates[1]] = "*"
     return ships, field
-
-
-
